61ff4eb0c6d9e3e0da4d83d2b07fdc5a58b9accb
LuisFernandoH <luisspartan10@gmail.com>
2024-03-07 09:44:26 -0600

    holamundo primmer comit

---
+++ holaMundo.js
@@ -0,0 +1,19 @@
+//Llamamos el Modulo http
+const http = require ('http');
+//Establecemos la url o IP de nuestro Servidor
+const hostname = '192.168.1.118';
+//Establecemos el puerto de escucha
+const port = 3000;
+//Creamos una instancia HTTP con un reques y un response
+const server = http.createServer((req, res) => {
+    //El servidor respondera un codigo 200
+    res.statusCode = 200;
+    //El servidor respondera con un texto plano
+    res.setHeader ('Content-Type', 'text/plain');
+    //El servidor respondera el mensaje hola mundo
+    res.end('Hola Mundo\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log('El servidor se esta ejecutando en http://${hostname}:${port}/');
+});
