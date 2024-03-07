2b775563c1f741dcb6e683b4a5d1c7fcb94feacd
LuisFernandoH <luisspartan10@gmail.com>
2024-03-07 09:50:29 -0600

    holamundo segundo commit

--- holaMundo.js
+++ holaMundo.js
@@ -9,9 +9,9 @@ const server = http.createServer((req, res) => {
     //El servidor respondera un codigo 200
     res.statusCode = 200;
     //El servidor respondera con un texto plano
-    res.setHeader ('Content-Type', 'text/plain');
-    //El servidor respondera el mensaje hola mundo
-    res.end('Hola Mundo\n');
+    res.setHeader ('Content-Type', 'text/html');
+    //El servidor respondera el mensaje hola mundo usando html
+    res.end('<h1>Hola Mund</h1>');
 });
 
 server.listen(port, hostname, () => {
